f7ef14587742a58a58ebfb9da0f4c56a1845291c
Sarko <mistersarko02@gmail.com>
2025-08-03 01:13:36 +0500

    Update main.jsx

--- almarket-frontend/src/main.jsx
+++ almarket-frontend/src/main.jsx
@@ -1,11 +1,11 @@
-// almarket-frontend/src/main.jsx
-
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
+import App from "./App";
 import "./index.css";
+import "./i18n";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
