7edea9bf1b8de2689b157751f914c15b573dc043
Sarko <mistersarko02@gmail.com>
2025-08-03 19:36:18 +0500

    Create AdminDashboard.jsx

---
+++ almarket-frontend/src/admin/AdminDashboard.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const AdminDashboard = () => {
+  return (
+    <div className="p-4">
+      <h1 className="text-2xl font-bold mb-4">Admin Paneli</h1>
+      <p>Bu ýerden sargytlary dolandyryp bilersiňiz.</p>
+    </div>
+  );
+};
+
+export default AdminDashboard;
