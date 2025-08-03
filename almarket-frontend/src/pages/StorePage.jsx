704c728e0749f9d773a208bc2633c898dccc953a
Sarko <mistersarko02@gmail.com>
2025-08-03 14:16:43 +0500

    Create StorePage.jsx

---
+++ almarket-frontend/src/pages/StorePage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const StorePage = () => {
+  return (
+    <div className="p-4">
+      <h2 className="text-xl font-bold mb-4">Harytlar dükany</h2>
+      <p>Oýunlara degişli harytlar şu sahypada görkeziler.</p>
+    </div>
+  );
+};
+
+export default StorePage;
