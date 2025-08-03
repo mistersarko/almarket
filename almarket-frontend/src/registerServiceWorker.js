14ddccb07b8820ff6fb24ccc3dcf8306c474b753
Sarko <mistersarko02@gmail.com>
2025-08-04 02:24:51 +0500

    Create registerServiceWorker.js

---
+++ almarket-frontend/src/registerServiceWorker.js
@@ -0,0 +1,16 @@
+// registerServiceWorker.js
+
+export default function registerServiceWorker() {
+  if ('serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
+      navigator.serviceWorker
+        .register('/service-worker.js')
+        .then(registration => {
+          console.log('ServiceWorker registered: ', registration);
+        })
+        .catch(registrationError => {
+          console.log('ServiceWorker registration failed: ', registrationError);
+        });
+    });
+  }
+}
