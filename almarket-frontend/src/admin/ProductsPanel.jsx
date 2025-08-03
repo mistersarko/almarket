0fda7798b24c641108690cba754264ac581daa42
Sarko <mistersarko02@gmail.com>
2025-08-03 17:54:35 +0500

    Create ProductsPanel.jsx

---
+++ almarket-frontend/src/admin/ProductsPanel.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const ProductsPanel = () => {
+  return (
+    <div className="p-4">
+      <h2 className="text-xl font-semibold mb-4">Önümler Paneli</h2>
+      <p>Önümleri goşmak, üýtgetmek ýa pozmak üçin bu bölümi ulanyň.</p>
+    </div>
+  );
+};
+
+export default ProductsPanel;
