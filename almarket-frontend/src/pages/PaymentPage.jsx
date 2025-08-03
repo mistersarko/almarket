a4fcfd5c5d4323cd8265b7c124ce330ac1112a51
Sarko <mistersarko02@gmail.com>
2025-08-03 14:15:30 +0500

    Create PaymentPage.jsx

---
+++ almarket-frontend/src/pages/PaymentPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const PaymentPage = () => {
+  return (
+    <div className="p-4">
+      <h2 className="text-xl font-bold mb-4">Töleg sahypasy</h2>
+      <p>Töleg bilen baglanyşykly maglumatlar şu ýerde bolar.</p>
+    </div>
+  );
+};
+
+export default PaymentPage;
