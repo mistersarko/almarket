3342439c8bb8b76ca05946fab73891f160afe4cb
Sarko <mistersarko02@gmail.com>
2025-08-03 17:53:30 +0500

    Create Dashboard.jsx

---
+++ almarket-frontend/src/admin/Dashboard.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Dashboard = () => {
+  return (
+    <div className="p-4">
+      <h2 className="text-2xl font-bold mb-4">Admin Paneline Hoş Geldiňiz</h2>
+      <p>Bu ýerde sargytlar, ulanyjylar we önümler bilen dolandyryp bilersiňiz.</p>
+    </div>
+  );
+};
+
+export default Dashboard;
