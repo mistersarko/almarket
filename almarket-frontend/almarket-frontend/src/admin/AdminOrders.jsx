5f5d6408c3f19dd89994b4d56b50306e61494c40
Sarko <mistersarko02@gmail.com>
2025-08-03 18:06:02 +0500

    Create AdminOrders.jsx

---
+++ almarket-frontend/almarket-frontend/src/admin/AdminOrders.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const AdminOrders = () => {
+  return (
+    <div className="p-4">
+      <h2 className="text-xl font-bold mb-4">Admin Orders Page</h2>
+      {/* Order management UI will go here */}
+    </div>
+  );
+};
+
+export default AdminOrders;
