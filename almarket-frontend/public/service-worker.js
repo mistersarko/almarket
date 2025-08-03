d8d49778782f26c7e022c5221abe5432bdd882a2
Sarko <mistersarko02@gmail.com>
2025-08-04 02:27:04 +0500

    Create service-worker.js

---
+++ almarket-frontend/public/service-worker.js
@@ -0,0 +1,14 @@
+// service-worker.js
+
+self.addEventListener('install', event => {
+  console.log('Service worker installed');
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  console.log('Service worker activated');
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(fetch(event.request));
+});
