b243b4448d38cd6beedd2b6b4d89cba28fabc817
Sarko <mistersarko02@gmail.com>
2025-08-07 00:29:59 +0500

    Create BrandController.php

---
+++ almarket-backend/app/Http/Controllers/BrandController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\BrandRequest;
+use App\Http\Resources\BrandResource;
+use App\Models\Brand;
+use Illuminate\Http\Request;
+
+class BrandController extends Controller
+{
+    public function index()
+    {
+        $brands = Brand::all();
+        return BrandResource::collection($brands);
+    }
+
+    public function store(BrandRequest $request)
+    {
+        $data = $request->validated();
+        $brand = Brand::create($data);
+        return new BrandResource($brand);
+    }
+
+    public function show(Brand $brand)
+    {
+        return new BrandResource($brand);
+    }
+
+    public function update(BrandRequest $request, Brand $brand)
+    {
+        $data = $request->validated();
+        $brand->update($data);
+        return new BrandResource($brand);
+    }
+
+    public function destroy(Brand $brand)
+    {
+        $brand->delete();
+        return response()->json(null, 204);
+    }
+}
