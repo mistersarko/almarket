9016be77f00ab941dc2293450141b62f7c71cd3b
Sarko <mistersarko02@gmail.com>
2025-08-07 00:19:15 +0500

    Create AdminAttributeValueController.php

---
+++ almarket-backend/app/Http/Controllers/AdminAttributeValueController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\AttributeValueRequest;
+use App\Http\Resources\AttributeValueResource;
+use App\Models\AttributeValue;
+use Illuminate\Http\Request;
+
+class AdminAttributeValueController extends Controller
+{
+    public function index()
+    {
+        $values = AttributeValue::with('attribute')->get();
+        return AttributeValueResource::collection($values);
+    }
+
+    public function store(AttributeValueRequest $request)
+    {
+        $data = $request->validated();
+        $value = AttributeValue::create($data);
+        return new AttributeValueResource($value);
+    }
+
+    public function show(AttributeValue $attributeValue)
+    {
+        return new AttributeValueResource($attributeValue);
+    }
+
+    public function update(AttributeValueRequest $request, AttributeValue $attributeValue)
+    {
+        $data = $request->validated();
+        $attributeValue->update($data);
+        return new AttributeValueResource($attributeValue);
+    }
+
+    public function destroy(AttributeValue $attributeValue)
+    {
+        $attributeValue->delete();
+        return response()->json(null, 204);
+    }
+}
