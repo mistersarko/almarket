14b64cfc7e483bcba00f0fb77506622cdc964883
Sarko <mistersarko02@gmail.com>
2025-08-07 14:02:35 +0500

    Create almarket-backend/database/seeders/DatabaseSeeder.php

---
+++ almarket-backend/database/seeders/DatabaseSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        // Şu ýere goşmaça seeder klaslaryny çagyrmak bolýar
+        // $this->call(UserSeeder::class);
+    }
+}
