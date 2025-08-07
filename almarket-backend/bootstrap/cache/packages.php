96b783d057a6585bacdbd3d17f983282da003f35
Sarko <mistersarko02@gmail.com>
2025-08-07 15:49:17 +0500

    Create almarket-backend/bootstrap/cache/packages.php

---
+++ almarket-backend/bootstrap/cache/packages.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+    'providers' => [
+        // Cached service providers list
+    ],
+    'eager' => [
+        // Eager loaded providers
+    ],
+    'deferred' => [
+        // Deferred providers
+    ],
+    'when' => [
+        // When to load deferred providers
+    ],
+];
