5417fd1013d0e14f88730bdf8a3c5ee1bb550648
Sarko <mistersarko02@gmail.com>
2025-08-07 14:03:09 +0500

    Create almarket-backend/routes/api.php

---
+++ almarket-backend/routes/api.php
@@ -0,0 +1,13 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\UserController;
+
+Route::post('/login', [AuthController::class, 'login']);
+Route::post('/register', [AuthController::class, 'register']);
+
+Route::middleware('auth:api')->group(function () {
+    Route::get('/user', [UserController::class, 'profile']);
+});
