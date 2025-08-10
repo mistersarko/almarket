5e337507cbcc68a2a92de0c18e2847f7944655b2
Mister_Sarko <mistersarko02@gmail.com>
2025-08-10 15:31:58 +0500

    Update main.jsx
    Signed-off-by: Mister_Sarko <mistersarko02@gmail.com>

--- almarket-frontend/src/main.jsx
+++ almarket-frontend/src/main.jsx
@@ -4,10 +4,17 @@ import App from "./App";
 import "./index.css";
 import "./i18n";
 import registerServiceWorker from './registerServiceWorker';
+
+import { SpeedInsights } from '@vercel/speed-insights/react';  // <-- Muny goşduk
+
 registerServiceWorker();
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <>
+      <App />
+      <SpeedInsights />       {/* <-- Muny App-den soň goýduk */}
+    </>
   </React.StrictMode>
 );
